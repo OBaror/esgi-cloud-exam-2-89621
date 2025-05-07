641516327a843b3c98c1567133853e307b9b97bf
Baror Orone <obaror@myges.fr>
2025-05-07 12:45:34 +0200

    Initial commit2

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  'postgresql://esgi_cloud_exam_db_89621_user:4d6GPswP9hwOJWyvt5er6sZGkK1V9g0b@dpg-d0djgnhr0fns739adrtg-a/esgi_cloud_exam_db_89621', // TODO
   {
     dialectOptions: {
       ssl: {
